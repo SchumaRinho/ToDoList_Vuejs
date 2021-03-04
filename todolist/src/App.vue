8ca9161907c40befcb9dc491e0d928fce5ed92f8
Azaïs <cleme.bluefox.leroy@gmail.com>
2021-03-04 16:59:48 +0100

    refactor:

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -10,7 +10,7 @@
     <br><br>
     </li>
   </ul> 
-		
+  
   Nom : <input v-model="newTodo" placeholder="Nouvelle To Do">
   <button v-on:click="add">Ajouter</button>
 
@@ -22,64 +22,64 @@
 
 export default {
   name: 'App',
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          name : 'tache 1',
-          completed : false
-        },{
-          id: 2,
-          name : 'tache 2',
-          completed: true
-        },{
-          id: 3,
-          name : 'tache 3',
-          completed: true
-        },{
-          id: 4,
-          name : 'tache 4',
-          completed: false
-        },{
-          id: 5,
-          name : 'tache 5',
-          completed: false
-        }
-      ],
-      newTodo: '',
-      filter: 'all',
-    }
-  },
-  methods: {
-    remove(id){
-      let i = 0;
-      while(id!=this.todos[i].id){
-      i++;
-      }
-      this.todos.splice(i,1)
-    },
-    add(){
-      this.todos.push({id : this.todos.length+1, name : this.newTodo, completed : false});
-      console.log("Nouvelle todo ajouté avec succés : " + this.newTodo);
-    },
-    doneSwitch(id){
-      let i = 0;
-      while(id!=this.todos[i].id){
-        i++;
-      }
-      this.todos[i].completed = !this.todos[i].completed;
-    },
-    affiche(){
-      console.log(this.todos);
-      for(let todo in this.todos){
-        console.log(this.todos[todo]);
-      }
-    },
-  },
   components: {
-  //HelloWorld
-  }
+    //HelloWorld
+  },
+  data() {
+				return {
+					todos: [
+						{
+							id: 1,
+							name : 'tache 1',
+							completed : false
+						},{
+							id: 2,
+							name : 'tache 2',
+							completed: true
+						},{
+							id: 3,
+							name : 'tache 3',
+							completed: true
+						},{
+							id: 4,
+							name : 'tache 4',
+							completed: false
+						},{
+							id: 5,
+							name : 'tache 5',
+							completed: false
+						}
+					],
+					newTodo: '',
+					filter: 'all',
+				}
+			},
+			methods: {
+				remove(id){
+					let i = 0;
+					while(id!=this.todos[i].id){
+						i++;
+					}
+					this.todos.splice(i,1)
+				},
+				add(){
+					this.todos.push({id : this.todos.length+1, name : this.newTodo, completed : false});
+					console.log("Nouvelle todo ajouté avec succés : " + this.newTodo);
+				},
+				doneSwitch(id){
+					let i = 0;
+					while(id!=this.todos[i].id){
+						i++;
+					}
+					this.todos[i].completed = !this.todos[i].completed;
+				},
+				affiche(){
+					console.log(this.todos);
+					for(let todo in this.todos){
+						console.log(this.todos[todo]);
+					}
+				},
+			}
 }
 </script>
 
