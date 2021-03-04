4aac114ab93dc12e3154ef84b1cd69cf7b1fb2e9
Azaïs <cleme.bluefox.leroy@gmail.com>
2021-03-04 17:52:22 +0100

    feat: components

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,58 +1,95 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <ul style="list-style-type:none;">
+    <li v-for="todo in todos" :key="todo.id">
+      <div v-if="(filter=='all') || ( filter=='check' && todo.completed==true )">
+        <input type="checkbox" v-if="todo.completed" checked v-on:click="doneSwitch(todo.id)">
+        <input type="checkbox" v-else v-on:click="doneSwitch(todo.id)">
+        {{ todo.name }}
+        <button v-on:click="remove( todo.id )">Supprimer</button>
+      </div>
+    <br><br>
+    </li>
+  </ul>
+  
+  Nom : <input v-model="newTodo" placeholder="Nouvelle To Do">
+  <button v-on:click="add">Ajouter</button>
+
+  <br><br><button v-on:click="affiche">Afficher BdD</button>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'ToDoList',
   props: {
     msg: String
-  }
+  },
+  data() {
+				return {
+					todos: [
+						{
+							id: 1,
+							name : 'tache 1',
+							completed : false
+						},{
+							id: 2,
+							name : 'tache 2',
+							completed: true
+						},{
+							id: 3,
+							name : 'tache 3',
+							completed: true
+						},{
+							id: 4,
+							name : 'tache 4',
+							completed: false
+						},{
+							id: 5,
+							name : 'tache 5',
+							completed: false
+						}
+					],
+					newTodo: '',
+					filter: 'all',
+				}
+			},
+			methods: {
+				remove(id){
+					let i = 0;
+					while(id!=this.todos[i].id){
+						i++;
+					}
+					this.todos.splice(i,1)
+				},
+				add(){
+					this.todos.push({id : this.newId() + 1, name : this.newTodo, completed : false});
+				},
+				newId(){
+					let maxId = 1;
+					for(let todo in this.todos){
+						if(maxId < this.todos[todo].id){
+							maxId = this.todos[todo].id;
+						}
+					}
+					return maxId;
+				},
+				doneSwitch(id){
+					let i = 0;
+					while(id!=this.todos[i].id){
+						i++;
+					}
+					this.todos[i].completed = !this.todos[i].completed;
+				},
+				affiche(){
+					console.log(this.todos);
+					for(let todo in this.todos){
+						console.log(this.todos[todo]);
+					}
+				},
+			}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
