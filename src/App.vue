991e61309adaf9f294dad3d7d19d6eb0201a7cf7
Azaïs <cleme.bluefox.leroy@gmail.com>
2021-03-12 17:43:37 +0100

    feat: sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-<ToDoList />
+<div id="sidebar">
+  <sidebar/>
+</div>
+<div id="toDoList">
+  <ToDoList />
+</div>
 </template>
 
 <script>
 import ToDoList from './components/ToDoList.vue'
+import sidebar from './components/sidebar.vue';
+
 export default {
   name: 'App',
   components: {
-    ToDoList
+    ToDoList,
+    sidebar
   }
 }
 </script>
 
 <style>
-
+#app {
+  display:grid;
+  grid-template-columns: 25% 75%;
+  padding-left: 15px;
+  border: 1px solid black;
+}
+#sidebar{
+  border-right: 1px solid black;
+}
+#toDoList {
+  margin: 50px;
+}
+button{
+  background-color: gray;
+  border: none;
+  color: white;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+  margin: 5px 5px;
+}
 </style>
---
+++ src/components/sidebar.vue
@@ -0,0 +1,88 @@
+<template>
+<h1>Listes :</h1>
+
+  <ul>
+    <li v-for="list in todolists" :key=list.nom>
+      <button>{{list.nom}}</button>
+    </li>
+  </ul>
+
+<h4>Créer Liste :</h4>
+
+  Nom : <input v-model="newList" placeholder="Nom"><br><br>
+<button v-on:click="add">Créer la liste</button><br><br>
+
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+
+export default {
+  name: 'sidebar',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      newList: "",
+      todolists: [
+        {
+          nom: "listeBanale",
+          todos:[
+            {
+              id: 1,
+              name : 'tache 1',
+              completed : false
+            },{
+              id: 2,
+              name : 'tache 2',
+              completed: true
+            },{
+              id: 3,
+              name : 'tache 3',
+              completed: true
+            },{
+              id: 4,
+              name : 'tache 4',
+              completed: false
+            },{
+              id: 5,
+              name : 'tache 5',
+              completed: false
+            }
+          ],
+        },
+        {
+          nom: "autreListeBanale",
+          todos:[
+            {
+              id: 1,
+              name : 'tache A',
+              completed : false
+            },{
+              id: 2,
+              name : 'tache B',
+              completed: false
+            }
+          ],
+        }
+      ]
+    }
+  },
+  methods: {
+    ...mapActions("todolist", ['load']),
+    add(){
+      this.todolists.push({nom:this.newList, todos:[]});
+    },
+  },
+  computed: {
+    ...mapGetters("todolist", ["myGetter"]),
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
